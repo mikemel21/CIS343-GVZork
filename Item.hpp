49e766d8d544fa8ed4211e16b5f461d1529c0451
mikemel21 <mikemelei905@gmail.com>
2023-10-02 14:44:41 -0400

    More commenting

--- Item.hpp
+++ Item.hpp
@@ -3,7 +3,13 @@
 
 #include <iostream>
 #include <string>
-
+/* An Item is an object the player will encounter along the way;
+    Items have:
+        - a name (non-empty string)
+        - calorie amount (0 if not edible; cannot be < 0 or > 1000) : INT
+        - a weight (must be between 0 and 500) : FLOAT
+        - a description (non-empty string)
+*/
 class Item {
 public:
     std::string name;
@@ -15,6 +21,7 @@ public:
 };
 
 // stream overloader that will print/return Item in format NAME (X calories) - X lb - DESCRIPTION
+// Note: Used ChatGPT to gain an understanding of stream overloading but did not use it to create project-specific code
 std::ostream& operator<<(std::ostream& os, const Item& item);
 
 #endif
--- testmain.cpp
+++ testmain.cpp
@@ -1,5 +1,6 @@
 #include "Item.hpp"
-
+//! THIS IS JUST A TESTING FILE!!! DELETE WHEN FINISHED
+// NO PERMANENT CODE WILL GO HERE
 int main() {
     Item a("NAME", "this is a description", 300, 100);
     std::cout << a << std::endl;
