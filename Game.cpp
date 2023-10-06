527cdcb0b8037e9ed48bac1d1ce83a24138c8b0b
mikemel21 <mikemelei905@gmail.com>
2023-10-06 17:47:33 -0400

    Added all required Locations, NPCS, and items

--- Game.cpp
+++ Game.cpp
@@ -6,9 +6,6 @@ Game::Game () {
     create_world();
     locationVector = std::vector<std::reference_wrapper<Location> >(locs.begin(), locs.end());
     cur = random_location();
-    //curLoc = cur;
-    //cur = random_location();
-    //std::cout << curLoc << std::endl;
     weight = 0;
     elfCalorieGoal = 5000;
     commandMap = setup_commands();
@@ -24,48 +21,128 @@ void Game::create_world () {
     this->locs.push_back(Location("Carillon Clocktower", "Scares you with a bell every 15 minutes"));
     // Bridge -> locationVector[3]
     this->locs.push_back(Location("Little Mak Bridge", "Not so little when you're scared of heights"));
+    // Blue Connection -> locationVector[4]
+    this->locs.push_back(Location("The Blue Connecion", "Food, classrooms. There's always something in here for you."));
+    // Water fountain -> locationVector[5]
+    this->locs.push_back(Location("Water Fountain","I wonder if there are fish in there..."));
+    // Gym -> locationVector[6]
+    this->locs.push_back(Location("Gym", "You'll probably find protein powder in here."));
+    // Manitou -> locationVector[7]
+    this->locs.push_back(Location("Manitou Hall", "Still don't know the difference between this and Makinac hall"));
 
     // add clocktower as neighbor to Kirkhoff
     this->locs[0].add_location("North", locs[2]);
     // add Mak bridge as neighbor to Kirkhoff
-    this->locs[0].add_location("East", locs[3]);
+    this->locs[0].add_location("North East", locs[3]);
+    // Water fountain neighbor to Kirkhoff
+    this->locs[0].add_location("East", locs[5]);
+    // Gym neighbor to Kirkhoff
+    this->locs[0].add_location("Northwest", locs[6]);
+    // Blue Connection neighbor to Kirkhoff
+    this->locs[0].add_location("Southwest", locs[4]);
+
+    // add Manitou as a neighbor to Mak
+    this->locs[1].add_location("West", locs[7]);
+    // Bridge neighbor to Mak
+    this->locs[1].add_location("South", locs[3]);
+    
+    // Kirkhoff neighbor to clocktower
+    this->locs[2].add_location("South", locs[0]);
+    // Gym neighbor to clocktower
+    this->locs[2].add_location("West", locs[6]);
+
     // add Mak Hall as a neighbor to Mak Bridge
     this->locs[3].add_location("North", locs[1]);
+    // Water fountain neighbor to bridge
+    this->locs[3].add_location("South", locs[5]);
+    // Manitou neighbor to Bridge
+    this->locs[3].add_location("Northeast", locs[7]);
 
-    //Item Coffee ("Coffee", "The only Java I like", 20, 1.0f);
+    // Gym neighbor to Blue Connection
+    this->locs[4].add_location("North", locs[6]);
+    // Kirkhoff neighbor to Blue Connection
+    this->locs[4].add_location("Northeast", locs[0]);
 
-    // Kirkhoff Items
+    // Kirkhoff neighbor to waterfountain
+    this->locs[5].add_location("West", locs[0]);
+    // Bridge neighbor to waterfountain
+    this->locs[5].add_location("East", locs[3]);
+
+    // Kirkhoff neighbor to Gym
+    this->locs[6].add_location("Southeast", locs[0]);
+    // Clocktower neighbor to Gym
+    this->locs[6].add_location("East", locs[2]);
+    // Blue Connection neighbor to Gym
+    this->locs[6].add_location("South", locs[4]);
+
+    // Mak neighbor to Manitou
+    this->locs[7].add_location("East", locs[1]);
+    // Bridge neighbor to Manitou
+    this->locs[7].add_location("South", locs[3]);
+
+    //* Kirkhoff Items
+    // sushi
     this->locs[0].add_item(Item ("Sushi", "Raw Fish... ew", 500, 2.0f));
+    // club pamphlet
     this->locs[0].add_item(Item ("Club Pamphlet", "This is a pamphlet for the ... lettuce eating club???", 0, 1.0f));
+    // subway sandwich
+    this->locs[0].add_item(Item ("Subway sandwhich", "No matter how big it is I'm never full from it", 200, 2.5f));
 
-    // Mak Items
+    //* Mak Items
+    // laptop
     this->locs[1].add_item(Item ("Laptop", "It looks like whoever used this laptop has memory leaks in their C code.", 0, 3.0f));
+    // coffee
+    this->locs[1].add_item(Item ("Coffee", "The only Java I like", 20, 1.0f));
 
-    // Kirkhoff NPCs
+    //* Blue Connection Items
+    // Laker Bowl
+    this->locs[4].add_item(Item ("Laker Bowl", "Mashed potatoes, chicken, and corn", 400, 3.5f));
+    // Quesadilla
+    this->locs[4].add_item(Item ("Quesadilla", "The secret menu of the POD store", 450, 3.0f));
+
+    //* Gym Items
+    // Protein Powder
+    this->locs[6].add_item(Item ("Protein Powder", "Supposedly taste like chocolate but is more like chalk.", 250, 4.0f));
+    // Dumbbell
+    this->locs[6].add_item(Item ("Dumbbell", "Do I have to carry this around?", 0, 6.0f));
+
+    //* Waterfountain Items
+    // fish
+    this->locs[5].add_item(Item ("Rotting fish", "Oh, I guess there are fish in there... please don't eat this", 300, 2.0f));
+
+
+    //*Kirkhoff NPCs
     this->locs[0].add_npc(NPC ("Coffee Shop employee", "makes the best coffee in all of Kirkhoff"));
 
-    // Mak NPC's
-    this->locs[1].add_npc(NPC ("POD store employee", "supplier of energy drinks")); 
+    //*Mak NPC's
+    this->locs[1].add_npc(NPC ("POD store employee", "supplier of energy drinks"));
+    this->locs[1].add_npc(NPC ("Professor Woodring", "Will sometimes respond to 'Mustang'."));
+
+    //*Gym NPC's
+    this->locs[6].add_npc(NPC ("Gym bro", "I wonder if he knows where food is."));
+
+    //*Blue Connection NPC's
+    this->locs[4].add_npc(NPC ("Hearth employee", "Depending on their mood you either will get a big or small portion."));
 }
 
 void Game::talk (std::vector<std::string> target) {
     //std::cout << "talk" << std::endl;
     // Get NPCs in the current room using the get_npcs() method
-    std::vector<NPC> currentRoomNPCs = curLoc.get_npcs();
+    // std::vector<NPC> currentRoomNPCs = curLoc.get_npcs();
 
-    // Check if the provided NPC is in the current room 
-        bool in_room = false;
-        for (NPC& npc : currentRoomNPCs) { //not really sure how this 4 loop works
-            if (npc.getName().compare(target.) == 0) {
-                in_room = true;
-                std::cout << "You talk to " << target << ": " << npc.getMessage(npc.messageNum) << std::endl;
-                break;
-            }
-        }
+    // // Check if the provided NPC is in the current room 
+    //     bool in_room = false;
+    //     for (NPC& npc : currentRoomNPCs) { //not really sure how this 4 loop works
+    //         if (npc.getName().compare(target.) == 0) {
+    //             in_room = true;
+    //             std::cout << "You talk to " << target << ": " << npc.getMessage(npc.messageNum) << std::endl;
+    //             break;
+    //         }
+    //     }
 
-        if (!in_room) {
-            std::cout << target << " is not in the current room." << std::endl;
-        }
+    //     if (!in_room) {
+    //         std::cout << target << " is not in the current room." << std::endl;
+    //     }
 }
 
 void Game::meet (std::vector<std::string> target) {
@@ -88,8 +165,6 @@ std::map<std::string, void(Game::*)(std::vector<std::string>)> Game::setup_comma
     commands["meet"] = &Game::meet;
     commands["take"] = &Game::take;
     commands["give"] = &Game::give;
-
-    //*commandMap["talk"]({"test"});
     
     return commandMap;
 }
